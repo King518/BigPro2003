3194cd3c4aea4a0892ee60a070a175e489449a90
zhangsan <zhangsan@clf.edu.cn>
2021-01-05 16:20:03 +0800

    lll

---
+++ src/main/java/controller/FinishController.java
@@ -0,0 +1,15 @@
+package controller;
+
+/**
+ * author: 令狐冲
+ * date: 2021/1/5 16:18
+ * desc: use for java11
+ */
+public class FinishController {
+
+
+    public static void main(String[] args) {
+        System.out.println("heihei");
+    }
+
+}
