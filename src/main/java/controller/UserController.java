4fb87f4731e4d6ad7019161a0855394485e7b94b
zhangsan <zhangsan@clf.edu.cn>
2021-01-05 16:14:39 +0800

    diyicitijiao

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -6,4 +6,8 @@ package controller;
  * desc: use for java11
  */
 public class UserController {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
